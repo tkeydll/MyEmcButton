75612c8c5ec7cb346f49206cda89e04707bb4447
tatsuya kobayashi <tkey.dll@gmail.com>
2019-05-05 17:52:37 +0900

    update

---
+++ index.js
@@ -0,0 +1,28 @@
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
+const telFrom = process.env.TWILIO_TEL_FROM;
+const callTo = process.env.TWILIO_CALL_TO;
+const voiceXml = process.env.VOICE_XML;
+
+const client = require('twilio')(accountSid, authToken);
+
+exports.handler = (event, context, callback) => {
+    console.log(event);
+
+    let clickType = event.deviceEvent.buttonClicked.clickType;
+    console.log(`ClickType: ${clickType}`);
+    console.log(`Using message template ${voiceXml}`);
+    console.log(`Callig to ${callTo}...`);
+
+    client.calls.create({
+            url: voiceXml,
+            to: callTo,
+            from: telFrom,
+            method: 'GET',
+        },
+        (err, call) => {
+            console.log(call);
+        }
+    ).then(call => console.log(call.sid));
+
+};
